a2b5e519c635fe84615dc2c3d76f22370c180a6f
Kaan Berat TOKAT <kaanberattokat@gmail.com>
2023-09-28 23:16:29 +0300

    Added Recursive Binary Tree

---
+++ RecursiveBinaryTree/FindIndex.cs
@@ -0,0 +1,39 @@
+﻿
+namespace RecursiveBinaryTree;
+
+public class FindIndex
+{
+
+    public FindIndex()
+    {
+        int[] numbers = { 45, 54, 86, 85, 32, 69, 96, 35, 73, 18 };
+
+        var indexIteration = FindIndex_IT(numbers, 96, 0);
+        var indexRecursive = FindIndex_Recursive(numbers, 54, 0);
+
+    }
+    int FindIndex_Recursive(int[] numbers, int val, int startIndex)
+    {
+        // Exit Condition
+        if (startIndex >= numbers.Length)
+            return -1;
+
+        // Actual Process
+        if (numbers[startIndex] == val)
+            return startIndex;
+
+        // Re-Calling
+        return FindIndex_Recursive(numbers, val, startIndex + 1);
+    }
+
+
+    int FindIndex_IT(int[] numbers, int val, int startIndex)
+    {
+        for (int i = startIndex; i < numbers.Length - startIndex; i++)
+        {
+            if (numbers[i] == val)
+                return i;
+        }
+        return -1;
+    }
+}
--- RecursiveBinaryTree/Program.cs
+++ RecursiveBinaryTree/Program.cs
@@ -1,2 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Recursive Binary Tree
+
+/*           8
+          /    \   
+         3      10
+       /  \      \
+      1   6       14
+         /  \     / 
+        4   7    13 
+*/
+
+
+int[] datas =  { 8 , 3 , 10 , 1 , 6 , 4 ,7 ,10 , 14, 13};
+
+var rootNode = new Node(datas[0]); // rootNode ilk elemanla başlatılır.
+
+foreach (var data in datas.Skip(1)) // Ilk elemanı atlayarak (çünkü o zaten rootNode) diğer elemanları döngüye alırız.
+{
+    AddNode(rootNode, data);
+}
+
+Traverse(rootNode);
+
+var result = isContains(6, rootNode); 
+
+Console.ReadLine();
+
+bool isContains(int value, Node rootNode) 
+{
+    // Girilen sayının Node içerisinde olup olmadığını kontrol eder.
+    if (rootNode == null)
+        return false;
+
+    if (value == rootNode.Val)
+        return true;
+
+    if (value < rootNode.Val)
+        return isContains(value, rootNode.LeftNode);
+    
+    else
+        return isContains(value, rootNode.RightNode);
+}
+
+void Traverse(Node rootNode)
+{
+    if (rootNode == null)
+        return;
+
+    Traverse(rootNode.LeftNode);
+    Console.WriteLine(rootNode.Val);
+    Traverse(rootNode.RightNode);
+}
+
+void AddNode(Node rootNode, int val)
+{
+    if (val < rootNode.Val)
+    {
+        if (rootNode.LeftNode is null)
+        {
+            rootNode.LeftNode = new Node(val);
+            return;
+        }
+        else
+        {
+            AddNode(rootNode.LeftNode, val);
+        }
+    }
+    else
+    {
+        if (rootNode.RightNode is null)
+        {
+            rootNode.RightNode = new Node(val);
+            return;
+        }
+        else
+        {
+            AddNode(rootNode.RightNode, val);
+        }
+    }
+}
+
+class Node
+{
+    public Node(int val)
+    {
+        Val = val;
+    }
+    public int Val { get; set; }
+
+    public Node LeftNode { get; set; }
+    public Node RightNode { get; set; }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Index: {0}",index);
+//Console.ReadLine();
+
+
+
